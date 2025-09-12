1fb39c6376da1bae05407bfb1994c1574a8e562a
dev_nick <developernikhil28@gmail.com>
2025-09-12 22:16:45 +0530

    Updated Navbar.jsx

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -11,21 +11,7 @@ export default function Navbar() {
                 <a className="navbar-brand" href="#">
                     Full Stack Application
                 </a>
-                {
-                /* 
-                <button 
-                  className="navbar-toggler" 
-                  type="button" 
-                  data-bs-toggle="collapse" 
-                  data-bs-target="#navbarSupportedContent" 
-                  aria-controls="navbarSupportedContent" 
-                  aria-expanded="false" 
-                  aria-label="Toggle navigation"
-                >
-                  <span className="navbar-toggler-icon"></span>
-                </button>
-                */
-                }
+          
 
                 <Link className='btn btn-outline-light' to="/adduser">Add User</Link>
 
